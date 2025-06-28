3bc6e1f10fd13ee26968e0da90ac7cb1fdbcf672
SamHacker <xux510208@gmail.com>
2025-06-28 10:40:42 +0800

    ✨ 新增警告訊息以提醒使用者在開發者工具中粘貼內容的風險

---
+++ scripts/alarmshell.js
@@ -0,0 +1,10 @@
+console.warn("%c注意！", "color: red; font-size: 50px; font-weight: bold;");
+console.warn(
+  "%c如果您不清楚自己在做什麼，請勿在此處粘貼任何內容。",
+  "font-size: 20px;"
+);
+console.warn(
+  "%c這是一個瀏覽器功能，旨在供開發人員使用。將內容粘貼到此處可能會導致人員個人資料受到損害。",
+  "font-size: 20px;"
+);
+console.warn("%c如果您不確定，請勿在此處粘貼任何內容。");
